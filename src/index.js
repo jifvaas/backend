23ccbf92f19ae6f264d66299511c70511b577154
Sander Koenders <sanderkoenders@gmail.com>
2020-01-20 16:19:44 +0100

    Set default port to 8080 to match Dockerfile

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const getEntries = require('./entries');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const entries = getEntries(path.join(__dirname, '..', 'entries.txt'));
 
